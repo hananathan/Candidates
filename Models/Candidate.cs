3a172bafc4ef6f00192d15c88e750bdaa8bcb525
hananathan <132227990+hananathan@users.noreply.github.com>
2025-08-19 15:31:43 +0300

    Create Candidate.cs
    Add candidate structure

---
+++ Models/Candidate.cs
@@ -0,0 +1,8 @@
+public class Candidate
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int Age { get; set; }
+    public string Nationality { get; set; } = string.Empty;
+    public string ImageFileName { get; set; } = string.Empty;
+}
